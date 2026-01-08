f7c2e085929d7fcfc88d8e434ad3b1739cced7c5
XD-DISTRO <74779316+XD-DISTRO@users.noreply.github.com>
2026-01-08 07:52:21 -0600

    fixed navigation

--- BrandLogoApp/app/index.tsx
+++ BrandLogoApp/app/index.tsx
@@ -1,11 +1,33 @@
-import React from "react";
-import { Text, View } from "react-native";
-import defaultStyles from "./styles/defaultStyles";
+import { useRouter } from "expo-router";
+import { Button, StyleSheet, Text, View } from "react-native";
+import colors from "./styles/colors";
 
-export default function HomeScreen() {
+export default function IndexScreen() {
+  const router = useRouter();
+
   return (
-    <View style={defaultStyles.pageContainer}>
-      <Text style={defaultStyles.bodyText}>Best Home Page</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Welcome to the App</Text>
+      <Button
+        title="Enter App"
+        color={colors.primary}
+        onPress={() => router.push("/(tabs)/fast")}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.primaryDark,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: colors.textPrimary,
+    marginBottom: 20,
+  },
+});
